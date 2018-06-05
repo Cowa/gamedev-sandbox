1ff535c2451383583bcae4a09916744e12a2f4e5
Brice Thomas <brice.p.thomas@gmail.com>
2018-06-05 20:55:31 +0200

    Improve Window code

--- scripts/Window.gd
+++ scripts/Window.gd
@@ -1,31 +1,25 @@
 extends Control
 
 var pressed = false
-var dragging = false
 var offset = Vector2()
 
 #######
 
 func _gui_input(event):
-	if Input.is_action_just_pressed("left_click") and is_mouse_on_panel():
+	if Input.is_action_just_pressed("left_click") and is_mouse_on_title():
 		pressed = true
 		offset = compute_offset()
 	
 	elif Input.is_action_just_released("left_click"):
 		pressed = false
-		dragging = false
 	
 	elif pressed and event is InputEventMouseMotion:
-		dragging = true
-
-func _process(delta):
-	if dragging:
 		set_position(get_global_mouse_position() + offset)
 
 #######
 
-func is_mouse_on_panel():
-	return $Panel.get_global_rect().has_point(get_global_mouse_position())
+func is_mouse_on_title():
+	return $Title.get_global_rect().has_point(get_global_mouse_position())
 
 func compute_offset():
-	return $Panel.get_global_position() - get_global_mouse_position()
+	return $Title.get_global_position() - get_global_mouse_position()
